0470d5dc2e83b4502b073a9fe93dfc387562d244
Oindrila0101 <oindrilapramanik18@gmail.com>
2026-06-13 08:31:35 +0000

    variables declared

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=12890;
+let accountEmail="abc@google.com";
+var accountPassword="1234";
+accountcity="kolkata";
+let accountState;
+let age=40;
+//accountId=2 // not allowed
+
+accountEmail="hchc@hc.com"
+accountPassword="1903"
+accountCity="Bengaluru"
+console.log(accountId);
+//let age=45;
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState,age]);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
