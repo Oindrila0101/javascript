c11db07a10c74fedd2bc8ee44a0668222aad895a
Oindrila0101 <oindrilapramanik18@gmail.com>
2026-06-14 14:06:16 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello") // we are using nodejs,not browser
+console.log(6+6);
+
+let name="oindrila";
+let age=20;
+let isLoggedIn=false;
+let number = null;
+let state;
+
+
+//PRIMITIVE DATATYPES
+//number = 2 to pwr 53
+//bigint
+//string="women"
+//boolean=true/false
+//null=standalone value
+//undefined
+//symbol=unique
+
+//NONPRIMITIVE DATATYPES
+//object
+
+console.log(typeof "oindrila");
+console.log(typeof null);
+console.log(typeof undefined);
