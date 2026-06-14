30201a8709ab832a5a01db0ef2bc2937e21861b8
Oindrila0101 <oindrilapramanik18@gmail.com>
2026-06-14 14:50:47 +0000

    operations

--- .devcontainer/01_basics/03_conversionConfusion.js
+++ .devcontainer/01_basics/03_conversionConfusion.js
@@ -26,3 +26,37 @@ let somenumber=43;
 let stringnumber=String(somenumber);
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+//OPERATIONS
+
+let value=2;
+let negvalue=-value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*3);
+console.log(2**4);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1="hello";
+let str2="world";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+3);
+console.log(1+"3");
+console.log("1"+"3");
+console.log("1"+3+3);
+console.log(1+3+"3");
+console.log(3+4*4*2%6);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let counter=100;
+counter++;
+console.log(counter);
