8f20b91a879995dc45d6836fde2faf1eacb35d28
Oindrila0101 <oindrilapramanik18@gmail.com>
2026-06-14 14:29:41 +0000

    conversionconfusion

---
+++ .devcontainer/01_basics/03_conversionConfusion.js
@@ -0,0 +1,28 @@
+let score=22
+let num="331bc"
+
+console.log(typeof score);
+console.log(typeof(score));
+console.log(typeof num);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=33
+//"331abc"=NaN
+//true=1;false=0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=true;0=false
+//""=false
+//"oindrila=true"
+
+let somenumber=43;
+let stringnumber=String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
