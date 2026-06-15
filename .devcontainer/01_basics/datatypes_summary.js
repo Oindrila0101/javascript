dd31fd2b3277eaffe4e56314d14be4b8ace24a57
Oindrila0101 <oindrilapramanik18@gmail.com>
2026-06-15 04:17:27 +0000

    datatypes_summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// PRIMITIVE
+
+// 7 types= String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score=100;
+const scorevalue=100.4;
+
+const isLoggedIn=false;
+const outsideTemp=null;
+let userEmail;
+
+const id=Symbol('123');
+const anotherId=Symbol('123');
+
+console.log(id===anotherId);
+
+const bigNumber=12345678914566n;
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros=["shaktiman","nagraj","doga"];
+let myObj={
+    name:"oindrila",
+    age:20,
+}
+
+const myFunction=function(){
+    console.log("Hello worls");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
+console.log(typeof heros);
