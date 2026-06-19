889e1ed0213b343a7b6ea23077de7aea58989c99
Oindrila Pramanik <oindrilapramanik18@gmail.com>
2026-06-19 14:54:08 +0000

    objects

--- .devcontainer/02_basics/03_objects.js
+++ .devcontainer/02_basics/03_objects.js
@@ -33,4 +33,56 @@ jsuser.greetingtwo=function(){
 console.log(jsuser.greeting());
 console.log(jsuser.greetingtwo());
 
+//objects part 2
 
+const tinderuser=new Object();
+tinderuser.id="123abc";
+tinderuser.name="sammy";
+tinderuser.isloggedin=false;
+
+console.log(tinderuser);
+
+const regularuser={
+    email:"someone@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"sumita",
+            lastname:"pramanik"
+        }
+    }
+}
+
+console.log(regularuser.fullname);
+console.log(regularuser.fullname.userfullname);
+
+
+const obj1={1:"a",2:"b"};
+const obj2={3:"a",4:"b"};
+
+//const obj3=Object.assign({},obj1,obj2);
+const obj3={...obj1,...obj2};
+console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        email:"o@gmail.com"
+    },
+      {
+        id:1,
+        email:"o@gmail.com"
+    },
+      {
+        id:1,
+        email:"o@gmail.com"
+    },
+]
+
+users[1].email;
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isloggedin'));
