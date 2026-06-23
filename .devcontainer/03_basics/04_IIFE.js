cbf28e3bd6810c8eb6cdceaee79d662759129caf
Oindrila Pramanik <oindrilapramanik18@gmail.com>
2026-06-23 04:00:49 +0000

    IIFE

---
+++ .devcontainer/03_basics/04_IIFE.js
@@ -0,0 +1,24 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+/*function chai(){
+    console.log(`DB CONNECTED`);
+}
+chai();
+*/
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+(function aurcode(){
+    console.log(`DB CONNECTED TWO`);
+})();
+
+(()=>{
+    console.log(`DB CONNECTED THREE`);
+})();
+
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+})('oindrila');
