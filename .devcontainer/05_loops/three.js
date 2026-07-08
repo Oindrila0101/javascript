359cbb4730447f8ccd7c05d41c21dbdcba9ac8f7
Oindrila Pramanik <oindrilapramanik18@gmail.com>
2026-07-08 18:10:14 +0000

    map

--- .devcontainer/05_loops/three.js
+++ .devcontainer/05_loops/three.js
@@ -8,3 +8,33 @@ for (const num of arr) {
     console.log(num);
     
 }
+
+const greetings="Hello World";
+for(const greet of greetings){
+    console.log(`Each char is ${greet}`)
+}
+
+//maps
+
+const map=new Map()
+map.set('IN',"India");
+map.set('AUS',"Australia")
+map.set('FR',"France")
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+    
+}
+
+const myObject={
+    game1: 'NFS',
+    game2: 'Spiderman'
+
+}
+
+/*for(const [key,value] of myObject){
+    console.log(key,':-',value);
+
+    }
+    */
